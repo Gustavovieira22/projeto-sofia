904d5970be6be03ac7ee91ed310aac1caedaa3d2
Gustavovieira22 <gustavo-henryque1@hotmail.com>
2025-03-26 19:32:25 -0300

    comentários e documentação

--- gpt/gpt.js
+++ gpt/gpt.js
@@ -3,49 +3,42 @@ const dotenv = require("dotenv");
 const fs = require("fs");
 dotenv.config();
 
-//importando função de inicialização
-const connectDB = require('../config/database');
-const {rulesCache, menuCache, initData} = require('../data/dataLoader');//sincroniza os dados do banco em cache//
+//importando funções para inicialização do banco de dados//
+const connectDB = require('../config/database');//arquivo de conexão com o banco de dados//
+const {rulesCache, menuCache, initData} = require('../data/dataLoader');//sincroniza os dados do banco em cache local//
 
-async function gptParams() {
-  await connectDB(); // Aguarda a conexão com o banco
-  await initData();  // Só executa depois que o banco estiver pronto
-}
-
+//inicia função que carrega variaveis com parametros para configuração do gpt//
 gptParams();
 
-// Configuração da API
+// Configuração da API//
 const openai = new OpenAI({
-    apiKey:process.env.OPENAI_API_KEY
+    apiKey:process.env.OPENAI_API_KEY//Chave de API//
 });
 
-//variável para controlar o histórico
+//variável que controla parametros e histórico de conversas//
 const messages = [];
 
-// Função principal do chatbot
+// Função de chamada chatbot//
 async function gpt(message_body) {
-  //grava a mensagem do cliente no histórico
-  messages.push(...rulesCache);
-  messages.push(...menuCache);
-  messages.push({role: "user", content:message_body});
+  messages.push(...rulesCache);//grava regras de atendimento nos parametros do chatbot//
+  messages.push(...menuCache);//grava o conteúdo do cardápio nos parametros do chatbot//
+  messages.push({role: "user", content:message_body}); //grava a mensagem do cliente no histórico//
 
   try{
     const completion = await openai.chat.completions.create({
-        model: "gpt-4o",
+        model: "gpt-4o",       //modelo gpt usado no atendimento
         temperature: 0.3,      // Menos aleatório, mais focado
         top_p: 0.5,            // Garantindo diversidade controlada
         presence_penalty: 1.0, // Menos desvio para tópicos irrelevantes
         max_tokens: 300,       // Respostas detalhadas, mas não excessivas
-        messages: messages,
+        messages: messages,    //mensagens e parametros de funcionamento//
         // model: "o3-mini",
         // reasoning_effort: "low"
     });
     
-    //grava a resosta do chatbot no histórico
-    messages.push({role: "assistant", content:completion.choices[0].message.content});
-
+    messages.push({role: "assistant", content:completion.choices[0].message.content}); //grava a resosta do chatbot no histórico
     return completion.choices[0].message.content;
-
+    
   } catch (error) {
     console.error("Erro ao acessar a API:", error.response?.data || error.message);
     return `Erro ao processar resposta!`;
@@ -59,6 +52,13 @@ async function response_human(message) {
     return;
   }
   return;
+}
+
+/*Função gptParams() é responsável por chamar as funções de conexão e inicialização de variaveis de cache*/
+/*rulesCache e menuCache possuem os parametros necessários para a configuração do chatbot gpt*/
+async function gptParams() {
+  await connectDB(); // Aguarda a conexão com o banco de dados//
+  await initData();  // recupera os dados do banco e guarda em cache local//
 }
 
 module.exports = {gpt, response_human};
